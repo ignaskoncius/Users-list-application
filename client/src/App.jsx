3d325f9df6138bec6122519bcacf8804958f7e6b
Ignas Koncius <i.koncius@gmail.com>
2021-07-28 09:25:23 +0300

    create-react-app and clear

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <h2>App.jsx</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
