a47d6a779f3a3495918ac1e6369a6ebe2a034ad1
Ignas Koncius <i.koncius@gmail.com>
2021-07-28 09:34:04 +0300

    create userSchema

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: 'String',
+      required: true,
+    },
+    age: {
+      type: 'Number',
+      required: true,
+    },
+    email: {
+      type: 'String',
+      required: true,
+    },
+    password: {
+      type: 'String',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const UsersModel = mongoose.model('user', userSchema);
+
+module.exports = UsersModel;
