586ce4efcec907b829f4f1b0b67f77115d8f097a
Ignas Koncius <i.koncius@gmail.com>
2021-07-28 10:12:03 +0300

    cdm, getAllUsersFromDb functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import UsersForm from './components/usersForm';
+import { getAllUsers } from './utils/requests';
 
 class App extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = { allUsers: [] };
+  }
+
+  componentDidMount() {
+    this.getAllUsersFromDb();
+  }
+
+  getAllUsersFromDb = async () => {
+    const allUsers = await getAllUsers();
+    this.setState({ allUsers });
+    console.log(this.state.allUsers);
+  };
+
   render() {
     return (
       <div className="App container d-flex justify-content-center">
