97a9dd5a2ebab394b3c251fb760e097f5b8c5105
Ignas Koncius <i.koncius@gmail.com>
2021-07-28 10:40:03 +0300

    handleSubmit in usersForm works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
-          <UsersForm></UsersForm>
+          <UsersForm onGetAllUsersFromDb={this.getAllUsersFromDb}></UsersForm>
           <UsersList></UsersList>
         </div>
       </div>
--- client/src/components/usersForm.jsx
+++ client/src/components/usersForm.jsx
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class UsersForm extends Component {
-  state = {};
+  state = {
+    name: '',
+    age: '',
+    email: '',
+    password: '',
+  };
+
+  resetState = () => {
+    this.setState({ name: '', age: '', email: '', password: '' });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { name, age, email, password } = this.state;
+    const newUserObjToCreate = { name, age, email, password };
+    const ats = await axios.post('http://localhost:4000/users/new', newUserObjToCreate);
+    this.resetState();
+    this.props.onGetAllUsersFromDb();
+    console.log(ats.data);
+  };
+
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { state: s } = this;
     return (
       <div className="w-50">
         <h2>Create new user</h2>
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <input type="text" className="form-control" name="name" placeholder="Enter name" />
+            <input
+              onChange={this.handleInput}
+              value={s.name}
+              type="text"
+              className="form-control"
+              name="name"
+              placeholder="Enter name"
+            />
           </div>
           <div className="form-group">
-            <input type="number" className="form-control" name="age" placeholder="Enter age" />
+            <input
+              onChange={this.handleInput}
+              value={s.age}
+              type="number"
+              className="form-control"
+              name="age"
+              placeholder="Enter age"
+            />
           </div>
           <div className="form-group">
-            <input type="text" className="form-control" name="email" placeholder="Enter email" />
+            <input
+              onChange={this.handleInput}
+              value={s.email}
+              type="text"
+              className="form-control"
+              name="email"
+              placeholder="Enter email"
+            />
           </div>
           <div className="form-group">
-            <input type="text" className="form-control" name="password" placeholder="Enter password" />
+            <input
+              onChange={this.handleInput}
+              value={s.password}
+              type="text"
+              className="form-control"
+              name="password"
+              placeholder="Enter password"
+            />
           </div>
-          <button type="button" className="btn btn-success">
+          <button type="submit" className="btn btn-success">
             Submit form
           </button>
         </form>
