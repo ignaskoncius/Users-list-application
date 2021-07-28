cea3662948905955c669e83e1179a727a543ba31
Ignas Koncius <i.koncius@gmail.com>
2021-07-28 13:00:12 +0300

    handleEdit in OneUser,

--- client/src/components/OneUser.jsx
+++ client/src/components/OneUser.jsx
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
+import UsersForm from './UsersForm';
 
 class OneUser extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditOn: false,
+    };
+  }
+
+  handleEdit = (updatedUserData) => {
+    if (this.state.isEditOn) this.props.onUpdate(this.props.myUser._id, updatedUserData);
+    this.setState({ isEditOn: !this.state.isEditOn });
+  };
+
   render() {
     const { name, age, email, password, _id } = this.props.myUser;
     return (
       <div className="card m-2 w-25">
-        <div className="card-body">
-          <h5 className="card-title">Name: {name}</h5>
-          <p className="card-text">Age: {age}</p>
-          <p className="card-text">Email: {email}</p>
-          <p className="card-text">Password: {password}</p>
-        </div>
-        <div className="card-footer">
-          <button className="btn btn-success">Edit</button>
-          <button onClick={() => this.props.onDelete(_id)} className="btn btn-danger">
-            Delete
-          </button>
-        </div>
+        {this.state.isEditOn ? (
+          <UsersForm user={this.props.myUser} onEdit={this.handleEdit}></UsersForm>
+        ) : (
+          <React.Fragment>
+            <div className="card-body">
+              <h5 className="card-title">Name: {name}</h5>
+              <p className="card-text">Age: {age}</p>
+              <p className="card-text">Email: {email}</p>
+              <p className="card-text">Password: {password}</p>
+            </div>
+            <div className="card-footer">
+              <button onClick={this.handleEdit} className="btn btn-success">
+                Edit
+              </button>
+              <button onClick={() => this.props.onDelete(_id)} className="btn btn-danger">
+                Delete
+              </button>
+            </div>
+          </React.Fragment>
+        )}
       </div>
     );
   }
